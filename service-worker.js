6672581bceeb3fa39d3cd5e520697adece2014a4
ahewitt-glasswall <59472395+ahewitt-glasswall@users.noreply.github.com>
2020-07-24 15:25:39 +0000

    Deploying to gh-pages from  @ d1259df552c18e2d611effd6996f25858a303865 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.75139b8d7e3b5b1b9f389af83a7c9fbb.js"
+  "/mvp-rebuild-performance-tests/precache-manifest.2890300df1f8ecc67b06196dfc45839f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mvp-rebuild-performance-tests/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
