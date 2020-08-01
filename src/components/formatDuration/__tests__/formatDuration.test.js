295dc08ebe07f4a3bdd15b4145264ab2c1d4b456
ahewitt-glasswall <59472395+ahewitt-glasswall@users.noreply.github.com>
2020-08-01 22:24:24 +0100

    formatDuration tests

--- src/components/formatDuration/__tests__/formatDuration.test.js
+++ src/components/formatDuration/__tests__/formatDuration.test.js
@@ -1,5 +1,4 @@
 import formatDuration from "../formatDuration";
-import { format } from "path";
 
 test("Wrong_Input_Format_Throws_Error", () => {
     //Arrange
@@ -12,12 +11,62 @@ test("Wrong_Input_Format_Throws_Error", () => {
     expect(formatDuration).toThrow();
 });
 
+test("Input_With_Milliseconds", () => {
+    // Arrange
+    const inputDuration = "00:00:00.25";
+    const expectedOutput = "0.2500000";
 
-test("wagwan", () => {
-    const inputDuration = "00:00:25.4563837";
-
+    // Act
     let result = formatDuration(inputDuration);
 
-    console.info(result);
+    // Assert
+    expect(result).toEqual(expectedOutput);
 });
-//TODO: revisit these tests
+
+test("Input_With_Seconds", () => {
+    // Arrangea
+    const inputDuration = "00:00:25.0";
+    const expectedOutput = "25.0000000";
+
+    // Act
+    let result = formatDuration(inputDuration);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
+
+test("Input_With_Minutes", () => {
+    // Arrange
+    const inputDuration = "00:25:00.0";
+    const expectedOutput = "1500.0000000";
+
+    // Act
+    let result = formatDuration(inputDuration);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
+
+test("Input_With_Hours", () => {
+    // Arrange
+    const inputDuration = "25:00:00.0";
+    const expectedOutput = "90000.0000000";
+
+    // Act
+    let result = formatDuration(inputDuration);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
+
+test("Input_With_Seconds_And_Milliseconds", () => {
+    // Arrange
+    const inputDuration = "00:00:25.4563837";
+    const expectedOutput = "25.4563837";
+
+    // Act
+    let result = formatDuration(inputDuration);
+
+    // Assert
+    expect(result).toEqual(expectedOutput);
+});
