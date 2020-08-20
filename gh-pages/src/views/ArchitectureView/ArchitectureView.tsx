43a8ae8a8c31f836ff2d25ac109eda2e6ee03a9b
ahewitt-glasswall <ahewitt@glasswallsolutions.com>
2020-08-20 15:54:01 +0100

    updated serialize-javascript and architecture images on architecture view

--- gh-pages/src/views/ArchitectureView/ArchitectureView.tsx
+++ gh-pages/src/views/ArchitectureView/ArchitectureView.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-import ArchitectureImage from "./architecture.png";
-import DockerSequenceImage from "./dockerSequence.png";
 
 import styles from "./ArchitectureView.module.scss";
 
@@ -16,11 +14,13 @@ const ArchitectureView = (props: ArchitectureViewProps) => {
         <div className={styles.architecturePageContainer}>
             <h3>Executing a Test</h3>
             <hr />
-            <img src={ArchitectureImage} alt="Overall architecture for running a test."/>
+            <img src="https://raw.githubusercontent.com/ahewitt-glasswall/mvp-rebuild-performance-tests/master/documents/architecture/architecture.png"
+                alt="Overall architecture for running a test." />
 
             <h3>Inside the Docker Container</h3>
             <hr />
-            <img src={DockerSequenceImage} alt="Sequence diagram for the docker container tests run in." />
+            <img src="https://raw.githubusercontent.com/ahewitt-glasswall/mvp-rebuild-performance-tests/master/documents/architecture/dockerSequence.png"
+                alt="Sequence diagram for the docker container tests run in." />
         </div>
     );
 };
